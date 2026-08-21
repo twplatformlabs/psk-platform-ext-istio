7e2fa0c7c566635ff7bc0959523e731c95be3caf
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-08-21 12:21:17 -0500

    [nc] add base revision to the release, set on base deploy and used by ingressgateways
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/generate_istio_base_application.sh
+++ scripts/generate_istio_base_application.sh
@@ -10,6 +10,7 @@ echo "Generate Istio base and cni Application definition at base version: $istio
 
 mkdir -p deploy-files/istio-base
 mkdir -p deploy-files/istio-cni
+mkdir -p deploy-files/istio-revision-base
 
 # to generate a similar template for analysis
 # helm template istio-base istio/base \
@@ -125,6 +126,53 @@ spec:
         maxDuration: 5m
 EOF
 cat deploy-files/istio-cni/application.yaml
+
+echo "generating staged revision tag application.yaml"
+cat <<EOF > deploy-files/istiod-revision-base/application.yaml
+---
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: istio-revision-base
+  namespace: $argocd_namespace
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+  annotations:
+    argocd.argoproj.io/sync-wave: "5"
+spec:
+  project: psk-aws-control-plane-configuration
+
+  sources:
+    - repoURL: https://github.com/twplatformlabs/psk-platform-ext-istio
+      path: charts/istio-revision-base
+      targetRevision: HEAD
+      helm:
+        parameters:
+          - name: version
+            value: $istio_base_version
+          - name: revision
+            value: $base_revision
+    - repoURL: https://github.com/twplatformlabs/psk-aws-control-plane-configuration
+      targetRevision: HEAD
+      ref: config
+  destination:
+    server: https://kubernetes.default.svc
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    syncOptions:
+      - ServerSideApply=true
+      - SkipDryRunOnMissingResource=true
+    retry:
+      limit: 5
+      backoff:
+        duration: 30s
+        factor: 2
+        maxDuration: 5m
+EOF
+cat deploy-files/istiod-revision-base/application.yaml
+
 
 echo "copying istio base and cni environment values.yaml files"
 cp -v deploy-templates/istio-base-default-values.yaml deploy-files/istio-base/istio-base-default-values.yaml
